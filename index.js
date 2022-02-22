b5b556eb9175727e8db33e4cc7cc3dfb428aee38
Scott Green <sagreenxyz@gmail.com>
2022-02-21 19:31:26 -0600

    waypoint

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('HELLO WORLD!');
+    res.send('HELLO WORLD!  Again');
 })
 
-app.listen(3000, () => {
-    console.log('I am awake');
+app.listen(process.env.PORT, () => {
+    console.log('I am awake again!...');
 });
 
 // from the terminal, enter "node index.js"
