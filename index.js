72994d7d2479ee8dd597bc194bba7f52b5a50d14
Scott Green <sagreenxyz@gmail.com>
2022-02-21 19:11:44 -0600

    waypoint

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World - I am here');
+    res.send('Hello World - I am here - all good');
+})
+
+app.get('/second', function (req, res) {
+    res.send('Hello World - The second route');
 })
 
 app.listen(3000, () => {
@@ -11,4 +15,4 @@ app.listen(3000, () => {
 });
 
 // from the terminal, enter "node index.js"
-
+// or use nodemon to automatically refresh when changes are made
